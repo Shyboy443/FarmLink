8c3ccc74509ac48dd53f585757720f484ddc4e3e
shyboy443 <ashen.semithu@gmail.com>
2024-03-16 22:39:18 +0530

    Fixed and product commit

--- backend/Controllers/productController.js
+++ backend/Controllers/productController.js
@@ -64,8 +64,139 @@ const  createProduct = asyncHnadler(async (req,res) => {
 
 })
 
+// Get All products  In DB 
 
+const getAllProduct = asyncHnadler(async (req,res) => {
+    const products = await Product.find({}).sort("-createdAt") 
+    res.status(200).json(products)
+ })
 
-module.exports ={
-    createProduct,
-}
+// Get All products 
+
+const getProduct = asyncHnadler(async (req,res) => {
+    const products = await Product.find({user:req.user.id}).sort("-createdAt") // getting error since user not created
+    res.status(200).json(products)
+ })
+ 
+ // Get single product
+ 
+ const getSingleProduct = asyncHnadler(async (req,res) => {
+     const product = await Product.findById(req.params.id)
+     if(!product){
+         // if product doesnt exits
+         res.status(404)
+         throw new Error("Product not found")
+     }
+     // match product to its user
+     if(product.user.toString() !== req.user.id){
+         res.status(401)
+         throw new Error("User Not authorized")
+     }
+     res.status(200).json(product);
+ 
+ 
+ 
+ })
+ 
+ 
+ // Delete Product
+ 
+ const deleteProduct = asyncHnadler(async (req,res) => {
+ 
+     const product = await Product.findById(req.params.id)
+     if(!product){
+         // if product doesnt exits
+         res.status(404)
+         throw new Error("Product not found")
+     }
+     // match product to its user
+     if(product.user.toString() !== req.user.id){
+         res.status(401)
+         throw new Error("User Not authorized")
+     }
+     await product.deleteOne();
+     res.status(200).json({
+         message: "Product Deleted"
+     });
+ 
+ })
+ 
+ // Update Product
+ 
+ const  updateProduct = asyncHnadler(async (req,res) => {
+ 
+     const {name,category,quantity,price,description} = req.body;
+     const {id} = req.params;
+ 
+     const product = await Product.findById(req.params.id)
+ 
+      // if product doesnt exits
+     if(!product){
+    
+         res.status(404)
+         throw new Error("Product not found")
+ 
+     }
+        // match product to its user
+        if(product.user.toString() !== req.user.id){
+         res.status(401)
+         throw new Error("User Not authorized")
+     }
+ 
+ 
+     // Handle Image upload
+     let fileData = {};
+ 
+     if(req.file){
+      // Save Image to cloudinary
+         let uploadedFile;
+         try {
+             uploadedFile = await cloudinary.uploader.upload(req.file.path,{
+                 folder:"ShyApp1",
+                 resource_type:"image",
+             })
+         } catch (error) {
+             res.status(500)
+             throw new Error("Image could not be uploaded")
+         }
+ 
+ 
+         fileData = {
+             fileName: req.file.originalname, 
+             filePath: uploadedFile.secure_url,
+             fileType: req.file.mimetype, 
+             fileSize: fileSizeFormatter(req.file.size,2) ,
+         }
+     }
+ 
+ 
+     //Update product
+     const updateProduct = await Product.findByIdAndUpdate(
+         {_id: id},
+         {
+             name,
+             category,
+             quantity,
+             price,
+             description,
+             image: Object.keys(fileData).length === 0 ? product?.image :fileData ,
+         },
+         {
+             new: true,
+             runValidators: true,
+         }
+     )
+ 
+ 
+     res.status(200).json(updateProduct)
+ 
+ })
+ 
+ module.exports = {
+     createProduct,
+     getProduct, 
+     getSingleProduct, 
+     deleteProduct,
+     updateProduct,
+     getAllProduct
+ }
